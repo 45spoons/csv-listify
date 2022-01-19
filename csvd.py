d2b6e7cd78624db0c1a457b72d050b0e0ed8402b
45spoons <73328090+45spoons@users.noreply.github.com>
2022-01-20 00:13:52 +0200

    Directory is now fetched, separator in own variable
    Directory is now fetched, files can be anywhere supposedly
    Separator in own variable, added TODO: take sys.argv[3] and use it as the separator character, "," if undefined
    Idea: input and output separator are optionally different

--- csvd.py
+++ csvd.py
@@ -1,15 +1,21 @@
 import sys
+workingDirectory = sys.path[0] + "\\" ## Get path to the directory the code was called from
+inputPath = workingDirectory + sys.argv[1]
+outputPath = workingDirectory + sys.argv[2]
+sepChar = "," ## TODO: take sys.argv[3] and use it as the separator character, "," if undefined
 
-inputfile = open(sys.argv[1],"r")
-outputfile = open(sys.argv[2],"w")
+if (inputPath != outputPath):
+    try:
+        inputfile = open(inputPath,"r")
+        outputfile = open(outputPath,"w")
 
-lines = inputfile.readlines() ## Take whole file as an array of strings
-for line in lines:
-    words = line.rstrip("\n").split(sep=",") ## Strip away newline then split into array
-    name = words[0] ## First word in line is the name
-    for word in words:
-        if (word and word != name): ## Exclude empty words and the name
-            outputfile.write(name + "," + word + "\n") ## Write to .csv format
-
-inputfile.close()
-outputfile.close()
+        lines = inputfile.readlines() ## Take whole file as an array of strings
+        for line in lines:
+            words = line.rstrip("\n").split(sep=sepChar) ## Strip away newline then split into array
+            name = words[0] ## First word in line is the name
+            for word in words:
+                if (word and word != name): ## Exclude empty words and the name
+                    outputfile.write(name + sepChar + word + "\n") ## Write to .csv format                    
+    finally:
+        inputfile.close()
+        outputfile.close()
